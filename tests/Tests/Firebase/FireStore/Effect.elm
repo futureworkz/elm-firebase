ae7946fd242c3e52bcb6d61baba2a05d2cf7e685
Steven Yap <stevenyap@futureworkz.com>
2018-02-06 16:27:38 +0700

    Add failing test cases

---
+++ tests/Tests/Firebase/FireStore/Effect.elm
@@ -0,0 +1,36 @@
+module Tests.Firebase.FireStore.Effect exposing (..)
+
+import Test exposing (..)
+import Expect exposing (..)
+import Native.Firebase
+import Firebase.FireStore.Effect exposing (..)
+
+
+docOne : CreateSubMsg DocumentSnapshot
+docOne =
+    OnDocSnapshot (Doc "/one/one") identity
+
+
+collectionOne : CreateSubMsg QuerySnapshot
+collectionOne =
+    OnQuerySnapshot (Collection [] "/one") identity
+
+
+suite : Test
+suite =
+    describe "#FireStore.Effect"
+        [ test "removeSubs" <|
+            \_ ->
+                let
+                    state =
+                        [ "/one/one"
+                        , "/one"
+                        ]
+
+                    subs =
+                        []
+                in
+                    removeSubs subs state
+                        |> List.length
+                        |> equal 0
+        ]

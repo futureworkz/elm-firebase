9fc813baeb09672926e65517b637af53ef49c9d6
Steven Yap <stevenyap@futureworkz.com>
2018-03-12 16:48:15 +0700

    Add exposing firebase for external usage

--- src/Native/Firebase.js
+++ src/Native/Firebase.js
@@ -1,6 +1,10 @@
 // TODO: Change to package name space when published
 // var _futureworkz$elm_firebase$Native_Firebase = function() {
 var _user$project$Native_Firebase = function() {
+  // attach firebase to global window
+  // so other user can use it in their ports
+  window.firebase = firebase
+
   function initializeApp(config) {
     firebase.initializeApp({
       apiKey: config.apiKey,
